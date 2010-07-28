c34bdf84116f778ae717da5eae3cb1e91d9c4f44
Ivan Krasin <krasin@google.com>
2010-07-28 17:23:20 +0200

    Implement nearest live proxy selection.
    R=imkrasin
    CC=gloom, gloom.msk
    http://codereview.appspot.com/1864047

--- src/naclssh/web2socket.js
+++ src/naclssh/web2socket.js
@@ -7,7 +7,35 @@
 
 web2socket = new Object();
 
+web2socket.uris = [
+  "ws://127.0.0.1:10101/echo",
+];
+
+web2socket.preferredIndex = -1;
+
 web2socket.getProxyURI = function() {
-  return "ws://127.0.0.1:10101/echo";
+  var index = web2socket.preferredIndex;
+  if (index == -1) {
+    index = 0;
+  }
+  return web2socket.uris[index];
 };
 
+web2socket.tryProxy = function(index) {
+  var ws = new WebSocket(web2socket.uris[index]);
+
+  ws.onopen = function() {
+    if (web2socket.preferredIndex == -1) {
+      web2socket.preferredIndex = index;
+    }
+    ws.close();
+  };
+}
+
+web2socket.findNearestProxy = function() {
+  for (var i = 0; i < web2socket.uris.length; i++) {
+    web2socket.tryProxy(i);
+  }
+};
+
+web2socket.findNearestProxy();
