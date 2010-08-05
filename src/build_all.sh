514e5abca0e815b1bdcc43069683a7a661ab9656
Kate Volkova <gloom.msk@gmail.com>
2010-08-05 18:37:06 +0000

    Build for x86-64
    R=krasin
    CC=gloom.msk
    http://codereview.appspot.com/1926041

--- src/build_all.sh
+++ src/build_all.sh
@@ -8,6 +8,13 @@ function die() {
 if [ -z $NACL_SDK_ROOT ] ; then die "NACL_SDK_ROOT variable should be set" ; fi
 #export NACL_SDK_ROOT=/home/gloom/Projects/nativeclient/native_client/toolchain/linux_x86/sdk/nacl-sdk
 
+if [ -z $1 ]
+then
+	die "-m32 or -m64 should be specified"
+else
+	if [[ $1 == -m32 || $1 == -m64 ]] ; then MACHINE_FLAG=$1 ; else die "wrong flag: should be -m32 or -m64" ; fi
+fi
+
 NACL_CC_PREFIX=$NACL_SDK_ROOT/bin/nacl-
 
 DIR=$(pwd)
@@ -18,6 +25,7 @@ DIR=$(pwd)
 # src/patches/*.diff
 # downloads into src/third_party/
 
+export CFLAGS=${MACHINE_FLAG}
 
 make -C naclstubs || die "naclstubs make failed"
 
@@ -30,16 +38,24 @@ tar -xzf openssl-1.0.0a.tar.gz || die "tar openssl failed"
 patch -p1 < ../patches/libcrypto.diff || die "patch crypto failed"
 
 cd openssl-1.0.0a
-MACHINE=i686 ./config --cross-compile-prefix=$NACL_CC_PREFIX no-dso no-shared no-hw no-sse2 no-krb5 no-asm -I$DIR/naclstubs/include || die "config openssl failed"
+MACHINE=i686 ./config $MACHINE_FLAG --cross-compile-prefix=$NACL_CC_PREFIX no-dso no-shared no-hw no-sse2 no-krb5 no-asm -I$DIR/naclstubs/include || die "config openssl failed"
 
 make build_crypto || die "make build_crypto failed"
 
 
 # TMP HACK
 # TODO modify libssh2 config - no check for libssl is needed
-mkdir lib
-cp libcrypto.a lib/libssl.a
-cp libcrypto.a lib/libcrypto.a
+if [[ $MACHINE_FLAG == -m32 ]]
+then
+	mkdir lib
+	cp libcrypto.a lib/libssl.a
+	cp libcrypto.a lib/libcrypto.a
+else
+	mkdir lib64
+	cp libcrypto.a lib64/libssl.a
+	cp libcrypto.a lib64/libcrypto.a
+fi
+
 
 cd .. # in third_party
 
@@ -49,6 +65,12 @@ tar -xzf libssh2-1.2.6.tar.gz || die "tar libssh failed"
 
 patch -p1 < ../patches/libssh.diff || die "patch libssh failed"
 cd libssh2-1.2.6
+
+# CFLAGS passing to libssh2 problem: no other way to pass -m64 to configure (??)
+if [[ $MACHINE_FLAG == -m64 ]]
+then
+	NACL_CC_PREFIX=$NACL_SDK_ROOT/bin/nacl64-
+fi
 
 export CC=${NACL_CC_PREFIX}gcc
 export AR=${NACL_CC_PREFIX}ar
@@ -66,11 +88,18 @@ wget http://zlib.net/zlib-1.2.5.tar.gz || die "cannot download zlib"
 tar -xzf zlib-1.2.5.tar.gz || die "tar zlib failed"
 cd zlib-1.2.5
 
-export CFLAGS="-Dunlink=puts"
+export CFLAGS="-Dunlink=puts "${MACHINE_FLAG}
 ./configure --static || die "configure zlib failed"
 make || die "make zlib failed"
 
 cd ../../naclssh
 
-make || die "make ssh_plugin failed"
+make clean
+
+if [[ $MACHINE_FLAG == -m32 ]]
+then
+	make ssh_plugin_x86_32.nexe || die "make ssh_plugin failed"
+else
+	make ssh_plugin_x86_64.nexe || die "make ssh_plugin failed"
+fi
 
